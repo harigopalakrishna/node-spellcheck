0463927fb4e539d705f0aba0b04c00a2625f9876
Hari Gopalakrishna <hari@harimac.local>
2014-10-16 22:02:38 -0600

    added spell check module and implemented as /spellcheck/:id endpoint

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/spellcheck/:id',function(request,response){
+
+var SpellCheck = require('spellcheck'),
+      base = __dirname + (process.platform === 'win32' ? '\\' : '/'),
+      spell = new SpellCheck(base + 'en_US/en_US.aff', base + 'en_US/en_US.dic');
+
+spell.check(request.params.id, function(err, correct, suggestions) {
+    if (err) throw err;
+    if (correct)
+      response.send(
+		  '{"isMisspelled":false}'
+	  );
+    else
+      response.send(
+		   '{"isMisspelled":true}'
+	  );
+});
+
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
